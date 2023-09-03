6ed4340db2d71dacaa604202e14363a14682cf9b
sarahyasser <sarahyasserxo@gmail.com>
2023-09-04 01:46:34 +0300

    search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppLayoutModule } from './layout/app.layout.module';
 import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { MultiSelectModule } from 'primeng/multiselect';
+
+
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AppLayoutModule,
-    ButtonModule
+    ButtonModule,
+    InputTextModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MultiSelectModule,
+    BrowserAnimationsModule,
+    DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/test-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class TestServiceService {
+
+  private apiUrl = 'https://localhost:7288/Recipe'; 
+
+  constructor(private http: HttpClient) { }
+
+  searchByRecipeName(recipeName : string) : Observable<any> {
+    return this.http.get(`${this.apiUrl}/GetRecipesByName/${recipeName}`);
+  }
+}
